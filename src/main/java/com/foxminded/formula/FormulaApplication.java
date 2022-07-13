0ae5439acb56dbe48e3f938bf9e36c72eed91b3f
Valhuk <vladrockyvlad@gmail.com>
2022-07-14 00:25:00 +0300

    fixed problems with maven

--- src/main/java/com/foxminded/formula/FormulaApplication.java
+++ src/main/java/com/foxminded/formula/FormulaApplication.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import com.foxminded.formula.managers.ConsoleManager;
 
+
 public class FormulaApplication {
 
 	public static void main(String[] args) throws IOException {
