93ae937e08e656e3587d13433b3fd52f2ea6db22
Valhuk <vladrockyvlad@gmail.com>
2022-07-16 18:15:48 +0300

    fixed test + consolemanager + im using java 17

--- src/main/java/com/foxminded/formula/managers/ConsoleManager.java
+++ src/main/java/com/foxminded/formula/managers/ConsoleManager.java
@@ -7,16 +7,14 @@ import java.io.InputStreamReader;
 import com.foxminded.formula.formatters.TableFormatter;
 
 public class ConsoleManager {
-	
+
 	private TableFormatter table = new TableFormatter();
 	private FileReader reader = new FileReader();
 	private RacersResolver resolver = new RacersResolver(reader);
-	
-	public void run() throws IOException {
-		try(InputStreamReader streamReader = new InputStreamReader(System.in);
-		BufferedReader bufferedReader = new BufferedReader(streamReader);) {
-		try {
 
+	public void run() throws IOException {
+
+		try {
 			reader.setAbbreviations("abbreviations.txt");
 			reader.setEndLog("end.log");
 			reader.setStartLog("start.log");
@@ -27,21 +25,24 @@ public class ConsoleManager {
 			System.out.println("Error has been occured: " + ex.getLocalizedMessage());
 			ex.printStackTrace();
 		} finally {
-			System.out.println("\nDo you want to try again? (Y/n)");
-			String confirm = bufferedReader.readLine();
-			if (confirm.equalsIgnoreCase("Y")) {
-				run();
-			} else if (confirm.equalsIgnoreCase("n")) {
-				System.out.println("Exiting...");
-				bufferedReader.close();
-				streamReader.close();
-			} else {
-				System.out.println("Could not resolve the answer. Exiting...");
-				bufferedReader.close();
-				streamReader.close();
+			try (InputStreamReader streamReader = new InputStreamReader(System.in);
+					BufferedReader bufferedReader = new BufferedReader(streamReader);) {
+				System.out.println("\nDo you want to try again? (Y/n)");
+				String confirm = bufferedReader.readLine();
+				if (confirm.equalsIgnoreCase("Y")) {
+					System.runFinalization();
+					run();
+				} else if (confirm.equalsIgnoreCase("n")) {
+					System.out.println("Exiting...");
+					bufferedReader.close();
+					streamReader.close();
+				} else {
+					System.out.println("Could not resolve the answer. Exiting...");
+					bufferedReader.close();
+					streamReader.close();
+				}
 			}
 		}
-	}
 	}
 
 }
--- src/test/java/com/foxminded/formula/managers/RacersResolverTest.java
+++ src/test/java/com/foxminded/formula/managers/RacersResolverTest.java
@@ -2,28 +2,51 @@ package com.foxminded.formula.managers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.foxminded.formula.models.Racer;
 
+@ExtendWith(MockitoExtension.class)
 class RacersResolverTest {
 	
-	private FileReader reader = new FileReader();
+	@Mock
+	private FileReader reader;
+	
+	@InjectMocks
 	private RacersResolver resolver = new RacersResolver(reader);
 
 	@Test
 	void createSortedListOfRacers_ShouldReturnSortedListOfRacers_WhenGivenTimesAndAbbreviations() {
+		
 		reader.setAbbreviations("abbreviationsTest.txt");
 		reader.setEndLog("endTest.log");
 		reader.setStartLog("startTest.log");
-		List<Racer> actual = resolver.createListOfRacers();
+		LinkedList<Racer> racers = new LinkedList<Racer>();
+		racers.add(new Racer("SVF", "Sebastian Vettel", "FERRARI"));
+		racers.add(new Racer("DRR", "Daniel Ricciardo", "RED BULL RACING TAG HEUER"));	
+		HashMap<String, Long> startTime = new HashMap<String, Long>();
+		HashMap<String, Long> endTime = new HashMap<String, Long>();
+		startTime.put("SVF", 1527152578917L);
+		startTime.put("DRR", 1527153252054L);
+		endTime.put("SVF", 1527152643332L);
+		endTime.put("DRR", 1527153324067L);
 		LinkedList<Racer> expected = new LinkedList<Racer>();
 		        expected.add(new Racer("SVF", "Sebastian Vettel", "FERRARI", 64415L));
-				expected.add(new Racer("DRR", "Daniel Ricciardo", "RED BULL RACING TAG HEUER", 72013L));
-		assertEquals(actual, expected);
+				expected.add(new Racer("DRR", "Daniel Ricciardo", "RED BULL RACING TAG HEUER", 72013L));				
+		Mockito.when(reader.getRacersFromFile()).thenReturn(racers);
+		Mockito.when(reader.getStartTime()).thenReturn(startTime);
+		Mockito.when(reader.getEndTime()).thenReturn(endTime);
+		List<Racer> actual = resolver.createListOfRacers();
+		assertEquals(expected, actual);
 	}
 
 
